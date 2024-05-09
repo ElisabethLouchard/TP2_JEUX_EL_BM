fc37f4d92487cc33f7981dfe8261ee1b0968b137
BrendonMorel <BrendonMorel@users.noreply.github.com>
2024-05-09 11:40:49 -0400

    Ajout du joueur et son animation

---
+++ ETU/Game/Player.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "AnimatedGameObject.h"
+struct Inputs;
+class Player :
+    public AnimatedGameObject
+{
+public:
+    Player();
+    virtual bool init(const ContentManager& contentManager) override;
+    bool update(float deltaT, const Inputs& inputs);
+};
---
+++ ETU/Game/ShipAnimation.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "InputBasedAnimation.h"
+
+class ShipAnimation : public InputBasedAnimation
+{
+public:
+	ShipAnimation(sf::Sprite& s);
+	bool init(const ContentManager& contentManager) override;
+	void adjustNextFrame(const Inputs& inputs) override;
+private:
+	int nbFrameInState;
+};
