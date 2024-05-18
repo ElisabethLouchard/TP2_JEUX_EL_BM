3ef410b400a24070bceeed4e17f628a099a41f48
BrendonMorel <BrendonMorel@users.noreply.github.com>
2024-05-18 12:32:38 -0400

    Ajout de l'animation 'scale' pour les bonus' a '

--- ETU/Game/Bonus.cpp
+++ ETU/Game/Bonus.cpp
@@ -5,15 +5,17 @@
 
 Bonus::Bonus(const sf::Vector2f& initialPosition)
 	: GameObject()
+	, timeSinceLaunch(0)
 {
 	setPosition(initialPosition);
 }
 
 Bonus::Bonus(const Bonus& src)
-	:GameObject(src), soundBonus(src.soundBonus)
+	:GameObject(src), soundBonus(src.soundBonus), timeSinceLaunch(src.timeSinceLaunch)
 {
 	GameObject::initialize(*src.getTexture(), src.getPosition());
 }
+
 Bonus& Bonus::operator=(const Bonus& rhs)
 {
 	if (this != &rhs)
@@ -28,11 +30,18 @@ Bonus& Bonus::operator=(const Bonus& rhs)
 bool Bonus::update(float elapsedTime)
 {
 	move(sf::Vector2f(0, 5));
-	if (getScale() == sf::Vector2f(4,4)) {
-		setScale(5, 5);
-	}
-	else {
-		setScale(4, 4);
+	timeSinceLaunch += elapsedTime;
+
+	if (timeSinceLaunch >= 0.2f)
+	{
+		timeSinceLaunch = 0.0f;
+
+		if (getScale() == sf::Vector2f(4, 4)) {
+			setScale(5, 5);
+		}
+		else {
+			setScale(4, 4);
+		}
 	}
 	return true;
 }
@@ -43,6 +52,7 @@ void Bonus::initialize(const sf::Texture& texture, const sf::Vector2f& initialPo
 	GameObject::initialize(texture, initialPosition);
 	soundBonus.setBuffer(sb);
 	setScale(4, 4);
+	timeSinceLaunch = 0.0f;
 }
 
 void Bonus::deactivate() 
