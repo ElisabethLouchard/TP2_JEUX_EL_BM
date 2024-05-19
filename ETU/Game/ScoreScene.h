6bb44e559f5da41c210235f803335df73c9cb4da
BrendonMorel <BrendonMorel@users.noreply.github.com>
2024-05-19 17:23:03 -0400

    Mise à jour : Affichage des noms + scores

--- ETU/Game/ScoreScene.h
+++ ETU/Game/ScoreScene.h
@@ -13,14 +13,21 @@ public:
     virtual void draw(sf::RenderWindow& window) const override;
     virtual bool init() override;
     virtual bool uninit() override;
+    bool isValidInput(const std::string& input);
     virtual bool handleEvents(sf::RenderWindow& window) override;
 private:
+    bool inputFinished;
     ScoreContentManager scoreContentManager;
     sf::Texture scoreBackgroundTexture;
     sf::Sprite scoreBackground;
     sf::Text title;
     sf::Text subtitle;
     sf::Text instructions;
+    std::string playerInput;
+    sf::Text inputText;
+    sf::Text scoreText;
+    std::vector<sf::Text> playerNames;
+    std::vector<sf::Text> playerScores;
     bool hasTransition = false;
 };
 
